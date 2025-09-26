71504e850443c08e6ff4fcbdcc8c256a2f8f993b
Eduarda Caniato <eduarda.caniato@estudante.ufjf.br>
2025-09-25 22:13:58 -0300

    add o base

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/ufjf-dcc207/ufjf-dcc207-2025-3-a-atv02-EduardaCaniato.git",
   plugins: [react()],
 })
